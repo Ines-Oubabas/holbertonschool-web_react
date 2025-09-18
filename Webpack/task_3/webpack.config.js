0d43ae97600820de79d04a88e944bd612515ed7b
Ines <inesoubabas56@gmail.com>
2025-09-18 17:38:37 +0200

    test vers 4

--- Webpack/task_3/webpack.config.js
+++ Webpack/task_3/webpack.config.js
@@ -1,35 +1,71 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    header: './modules/header/header.js',  // ⬅️ strings, comme le checker veut
-    body:   './modules/body/body.js',
-    footer: './modules/footer/footer.js',
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js"
   },
+  devtool: "inline-source-map",
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public'),   // <= attendu par le checker
+    filename: "[name].bundle.js",      // => header.bundle.js, body.bundle.js, footer.bundle.js
+    chunkFilename: "[name].bundle.js",
+    publicPath: ""                   // ne nettoie pas public à chaque build
   },
-  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: "./public",
+    port: 8564,
+    open: true
+  },
+
   module: {
     rules: [
-      { test: /\.css$/i, use: ['style-loader', 'css-loader'] },
-    ],
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "javascript/auto",
+        use: [
+          {
+            loader: "file-loader",
+            options: { name: "[name].[ext]", outputPath: "assets/" }
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              disable: true,                 // plus rapide en dev
+              mozjpeg: { progressive: true },
+              optipng: { enabled: false },
+              pngquant: { quality: [0.65, 0.9], speed: 4 },
+              gifsicle: { interlaced: false }
+            }
+          }
+        ]
+      }
+    ]
   },
-  optimization: { splitChunks: { chunks: 'all' } },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({ title: 'task_3', inject: 'body' }),
+    new HtmlWebpackPlugin({
+      title: "Holberton Dashboard",
+      filename: "index.html",
+      chunks: ["vendors", "header", "body", "footer"] // injecte tous les bundles
+    })
   ],
-  devServer: {
-    // Le checker attend "contentBase: ./public" et port 8564
-    contentBase: path.resolve(__dirname, 'public'),
-    port: 8564,
-    open: true,
-    // (optionnel) garde aussi la clé moderne si tu veux continuer à l'utiliser localement :
-    // static: path.resolve(__dirname, 'public'),
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendors",
+          enforce: true
+        }
+      }
+    }
   },
+  performance: { hints: false }
 };
