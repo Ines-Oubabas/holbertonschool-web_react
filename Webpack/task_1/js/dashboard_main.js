a0f338d1fd54d946b4275d2aba88f956d5ec8660
Ines <inesoubabas56@gmail.com>
2025-09-18 12:44:52 +0200

    task_1: set build='webpack' and use _.debounce

--- Webpack/task_1/js/dashboard_main.js
+++ Webpack/task_1/js/dashboard_main.js
@@ -1,22 +1,18 @@
 import $ from 'jquery';
-import { debounce } from 'lodash';
+import _ from 'lodash';
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
-
 const $btn = $('<button>Click here to get started</button>');
 $('body').append($btn);
-
-const $count = $("<p id='count'></p>");
-$('body').append($count);
-
+$('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
 let clicks = 0;
 function updateCounter() {
   clicks += 1;
-  $count.text(`${clicks} clicks on the button`);
+  $('#count').text(`${clicks} clicks on the button`);
 }
 
-// anti-spam: 500ms
-$btn.on('click', debounce(updateCounter, 500));
+// 500 ms
+$btn.on('click', _.debounce(updateCounter, 500));
