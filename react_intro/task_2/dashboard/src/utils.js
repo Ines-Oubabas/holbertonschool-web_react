c7e3fd0ac73a4d9961be9df6e0ec4a6dd194d10b
Ines <inesoubabas56@gmail.com>
2025-09-25 14:29:18 +0200

    Task 2: update Notifications (close button, list, priorities, latest notification)

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -3,3 +3,7 @@ export const getCurrentYear = () => new Date().getFullYear();
 
 export const getFooterCopy = (isIndex) =>
   isIndex ? 'Holberton School' : 'Holberton School main dashboard';
+
+// ▼ Nouveau pour la task 5
+export const getLatestNotification = () =>
+  '<strong>Urgent requirement</strong> - complete by EOD';
