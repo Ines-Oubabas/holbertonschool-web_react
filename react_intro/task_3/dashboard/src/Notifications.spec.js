9ab96d5a18e277ad33044381f77a4c3f34f4654b
Inès Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2025-09-26 14:22:33 +0200

    Update Notifications.spec.js

--- react_intro/task_3/dashboard/src/Notifications.spec.js
+++ react_intro/task_3/dashboard/src/Notifications.spec.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import Notifications from './Notifications.jsx';
+import Notifications from './Notifications';
 
 describe('Notifications', () => {
   // 1) Titre + bouton (ignore case) — rien d’autre dans ce test
