75609b9605f20089924361bcaf134c04bd321f9b
Ines <inesoubabas56@gmail.com>
2025-10-05 19:48:19 +0200

    task_1: add unit tests for Header, Login, Footer

---
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../Notifications/Notifications.css'; // garde le style des notifs
+
+import Notifications from '../Notifications/Notifications.jsx';
+import Header from '../Header/Header.jsx';
+import Login from '../Login/Login.jsx';
+import Footer from '../Footer/Footer.jsx';
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <Notifications />
+      <Header />
+      <Login />
+      <Footer isIndex />
+    </React.Fragment>
+  );
+}
