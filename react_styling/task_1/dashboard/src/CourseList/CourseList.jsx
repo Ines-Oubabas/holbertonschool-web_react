f1c362f47099007dd40a6de4cdd2216eb827bd97
Ines <inesoubabas56@gmail.com>
2026-03-14 00:18:40 +0100

    ptite modif

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -4,25 +4,29 @@ import WithLogging from '../HOC/WithLogging'
 function CourseList({ courses = [] }) {
   return (
     <div className="courses mx-auto my-32 w-4/5 h-[29vh]">
-      {courses.length > 0 ? (
-        <table id="CourseList" className="w-full border-collapse border border-gray-300">
-          <thead>
-            <CourseListRow textFirstCell="Available courses" isHeader={true} />
-            <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
-          </thead>
+      <table id="CourseList" className="w-full border-collapse border border-gray-300">
+        {courses.length > 0 ? (
+          <>
+            <thead>
+              <CourseListRow textFirstCell="Available courses" isHeader={true} />
+              <CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />
+            </thead>
+            <tbody>
+              {courses.map(course => (
+                <CourseListRow
+                  key={course.id}
+                  textFirstCell={course.name}
+                  textSecondCell={course.credit}
+                />
+              ))}
+            </tbody>
+          </>
+        ) : (
           <tbody>
-            {courses.map(course => (
-              <CourseListRow key={course.id} textFirstCell={course.name} textSecondCell={course.credit} />
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <table id="CourseList" className="w-full border-collapse border border-gray-300">
-          <thead>
             <CourseListRow isHeader={true} textFirstCell="No course available yet" />
-          </thead>
-        </table>
-      )}
+          </tbody>
+        )}
+      </table>
     </div>
   )
 }
