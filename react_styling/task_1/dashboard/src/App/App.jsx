163afaa4e0e61c91257892b3e7c1599def14ae86
Ines <inesoubabas56@gmail.com>
2026-03-14 15:49:24 +0100

    test

--- react_styling/task_1/dashboard/src/App/App.jsx
+++ react_styling/task_1/dashboard/src/App/App.jsx
@@ -10,15 +10,28 @@ import CourseList from '../CourseList/CourseList'
 import PropTypes from 'prop-types'
 import { getLatestNotification } from '../utils/utils'
 
+const notificationsList = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+]
+
+const coursesList = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+]
+
 class App extends React.Component {
   constructor(props) {
     super(props)
-    // This binding is necessary to make `this` work in the callback
     this.handleKeyDown = this.handleKeyDown.bind(this)
   }
 
   static defaultProps = {
+    isLoggedIn: true,
     logOut: () => {},
+    courses: coursesList,
   }
 
   componentDidMount() {
@@ -29,7 +42,7 @@ class App extends React.Component {
     document.removeEventListener('keydown', this.handleKeyDown)
   }
 
-  handleKeyDown = event => {
+  handleKeyDown(event) {
     if (event.ctrlKey && event.key === 'h') {
       alert('Logging you out')
       this.props.logOut()
@@ -37,44 +50,46 @@ class App extends React.Component {
   }
 
   render() {
-    const notificationsList = [
-      { id: 1, type: 'default', value: 'New course available' },
-      { id: 2, type: 'urgent', value: 'New resume available' },
-      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-    ]
-    const coursesList = [
-      { id: 1, name: 'ES6', credit: '60' },
-      { id: 2, name: 'Webpack', credit: '20' },
-      { id: 3, name: 'React', credit: '40' },
-    ]
-    const { isLoggedIn } = this.props
+    const { isLoggedIn, courses } = this.props
 
     return (
       <React.Fragment>
         <div className="root-notifications">
           <Notifications notifications={notificationsList} />
         </div>
+
         <Header />
+
         {isLoggedIn ? (
           <BodySectionWithMarginBottom title="Course list">
-            <CourseList courses={coursesList} />
+            <CourseList courses={courses} />
           </BodySectionWithMarginBottom>
         ) : (
           <BodySectionWithMarginBottom title="Log in to continue">
             <Login />
           </BodySectionWithMarginBottom>
         )}
+
         <BodySection title="News from the School">
           <p>Holberton School News goes here</p>
         </BodySection>
+
         <Footer />
       </React.Fragment>
     )
   }
 }
 
-export default App
-
 App.propTypes = {
+  isLoggedIn: PropTypes.bool,
   logOut: PropTypes.func,
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      credit: PropTypes.number.isRequired,
+    })
+  ),
 }
+
+export default App
--- react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseListRow.jsx
@@ -5,13 +5,13 @@ export default function CourseListRow({ isHeader = false, textFirstCell = '', te
     <tr className={isHeader ? 'bg-table-header opacity-66' : 'bg-table-rows opacity-45'}>
       {isHeader ? (
         textSecondCell === null ? (
-          <th colSpan="2" className="border border-gray-400">
+          <th colSpan="2" className="border border-gray-400 text-center">
             {textFirstCell}
           </th>
         ) : (
           <>
-            <th className="border border-gray-400">{textFirstCell}</th>
-            <th className="border border-gray-400">{textSecondCell}</th>
+            <th className="border border-gray-400 text-center">{textFirstCell}</th>
+            <th className="border border-gray-400 text-center">{textSecondCell}</th>
           </>
         )
       ) : (
