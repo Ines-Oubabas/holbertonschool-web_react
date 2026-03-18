069a38c2b713dcf76f5e867c4efd43a86d780040
Inès Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2026-03-18 22:50:08 +0100

    Refactor Footer component styling and structure

--- react_styling/task_4/dashboard/src/Footer/Footer.jsx
+++ react_styling/task_4/dashboard/src/Footer/Footer.jsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import { getCurrentYear, getFooterCopy } from '../utils/utils.js'
+import { getCurrentYear, getFooterCopy } from '../utils/utils';
 
-export default function Footer({ isIndex = true }) {
+export default function Footer() {
   return (
-    <div className="mt-8 border-t-2 border-[var(--main-color)] py-3 text-center text-sm italic max-[520px]:text-base">
-      <p>
-        Copyright {getCurrentYear()} - {getFooterCopy(isIndex)}
-      </p>
+    <div className="App-footer flex justify-center items-center border-t-4 border-[color:var(--main-color)] w-full mt-auto py-2">
+      <p className="italic text-xl p-1 text-center max-[520px]:text-lg max-[520px]:p-0 max-[450px]:text-[16px] max-[375px]:text-[15px]">Copyright {getCurrentYear()} - {getFooterCopy(false)}</p>
     </div>
   )
 }
