22c965be7ea7b7489b6256938312f4710c34165b
Inès Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2026-03-18 22:52:17 +0100

    Refactor BodySection component for improved styling

--- react_styling/task_4/dashboard/src/BodySection/BodySection.jsx
+++ react_styling/task_4/dashboard/src/BodySection/BodySection.jsx
@@ -1,18 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-function BodySection({ title, children = null }) {
+export default function BodySection({ title, children }) {
   return (
-    <div className="px-4 py-4">
-      <h2 className="mb-2 text-base font-bold">{title}</h2>
-      {children}
+    <div className='bodySection max-w-full'>
+      <h2 className="font-bold text-xl mt-8">{title}</h2>
+      <div className="max-w-full overflow-hidden">
+        {children}
+      </div>
     </div>
   )
 }
-
-BodySection.propTypes = {
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node,
-}
-
-export default BodySection
