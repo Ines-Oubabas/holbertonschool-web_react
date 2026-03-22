ce7642029ce5f227d3b9e517b9e952ddd1bd7509
Ines <inesoubabas56@gmail.com>
2026-03-22 15:29:18 +0100

    delete

--- react_hooks/task_4/dashboard/fileTransformer.js
+++
@@ -1,6 +0,0 @@
-// CommonJS transformer pour Jest (fichiers statiques)
-module.exports = {
-  process() {
-    return { code: 'module.exports = "test-file-stub";' };
-  }
-};
--- react_hooks/task_4/dashboard/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-
-export default defineConfig({
-  base: '/holbertonschool-web_react/',
-  plugins: [react(), tailwindcss()],
-});
