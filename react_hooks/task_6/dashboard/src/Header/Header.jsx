560c0be6ccb9ac2337c1068069c48239a77ca247
Inès Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2026-03-28 18:08:24 +0100

    Update Header.jsx

--- react_hooks/task_6/dashboard/src/Header/Header.jsx
+++ react_hooks/task_6/dashboard/src/Header/Header.jsx
@@ -1,23 +1,36 @@
-import holbertonLogo from '../assets/holberton-logo.jpg';
+import holbertonLogo from '../assets/holberton-logo.jpg'
 
-export default function Header({ user, logOut }) {
+function Header({ user, logOut }) {
   return (
     <>
-      <header className="App-header flex items-center p-[10px]">
+      <div className="App-header flex items-center py-2 max-[520px]:flex-col">
         <img
-          className="App-logo h-[200px] mr-5"
+          className="App-logo h-60 pointer-events-none max-[520px]:h-60"
           src={holbertonLogo}
-          alt="Holberton logo"
+          alt="holberton logo"
         />
-        <h1 className="text-[var(--main-color)] text-4xl font-bold">
-          School Dashboard
+        <h1 className="font-bold text-[color:var(--main-color)] text-5xl max-[520px]:text-5xl max-[520px]:mt-2 max-[435px]:text-4xl">
+          School dashboard
         </h1>
-      </header>
-      {user.isLoggedIn && (
-        <section id="logoutSection">
-          Welcome {user.email} (<a href="#" onClick={logOut}>logout</a>)
-        </section>
+      </div>
+
+      {user?.isLoggedIn && (
+        <div id="logoutSection" className="mt-2 text-right pr-4">
+          Welcome {user.email} (
+          <a
+            href="#"
+            onClick={(event) => {
+              event.preventDefault()
+              logOut()
+            }}
+          >
+            logout
+          </a>
+          )
+        </div>
       )}
     </>
-  );
+  )
 }
+
+export default Header
