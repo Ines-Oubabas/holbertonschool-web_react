be0f42ccdb5867db84425add5ddbd1da7d5b5431
Inès Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2026-03-28 18:08:59 +0100

    Update Footer.jsx

--- react_hooks/task_6/dashboard/src/Footer/Footer.jsx
+++ react_hooks/task_6/dashboard/src/Footer/Footer.jsx
@@ -1,11 +1,22 @@
-export default function Footer({ user }) {
-  const year = new Date().getFullYear();
+import { getCurrentYear, getFooterCopy } from '../utils/utils'
+
+function Footer({ isIndex = false, user }) {
+  const currentYear = getCurrentYear()
+  const footerCopy = getFooterCopy(isIndex)
+
   return (
-    <footer className="App-footer border-t-[3px] border-[var(--main-color)] py-2 text-center italic">
-      <p>Copyright {year} - Holberton School main dashboard</p>
-      {user && user.isLoggedIn && (
-        <p><a href="mailto:contact@holbertonschool.com">Contact us</a></p>
+    <div className="App-footer flex justify-center items-center border-t-4 border-[color:var(--main-color)] w-full mt-auto py-2">
+      <p className="italic text-xl p-1 text-center max-[520px]:text-lg max-[520px]:p-0 max-[450px]:text-[16px] max-[375px]:text-[15px]">
+        Copyright {currentYear} - {footerCopy}
+      </p>
+
+      {user?.isLoggedIn && (
+        <p className="ml-4">
+          <a href="#">Contact us</a>
+        </p>
       )}
-    </footer>
-  );
+    </div>
+  )
 }
+
+export default Footer
