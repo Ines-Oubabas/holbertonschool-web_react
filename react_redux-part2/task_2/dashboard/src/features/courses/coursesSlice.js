c3f7d895536f9d0c46bf0ec4a1a1ecfacad05bf5
Ines <inesoubabas56@gmail.com>
2026-04-03 13:17:49 +0200

    files for task 2

--- react_redux-part2/task_2/dashboard/src/features/courses/coursesSlice.js
+++ react_redux-part2/task_2/dashboard/src/features/courses/coursesSlice.js
@@ -1,9 +1,9 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { logout } from '../auth/authSlice'; 
+import { logout } from '../auth/authSlice';
 import axios from 'axios';
 
 const initialState = {
-  courses: [],
+    courses: [],
 };
 
 const API_BASE_URL = "http://localhost:5173";
@@ -11,7 +11,7 @@ const API_BASE_URL = "http://localhost:5173";
 const ENDPOINTS = { courses: `${API_BASE_URL}/courses.json` };
 
 const fetchCourses = createAsyncThunk(
-    'courses/fetchCourses', 
+    'courses/fetchCourses',
     async () => {
         const response = await axios.get(ENDPOINTS.courses);
 
@@ -23,16 +23,33 @@ const fetchCourses = createAsyncThunk(
 const coursesSlice = createSlice({
     name: 'courses',
     initialState,
+    reducers: {
+        selectCourse: (state, { payload }) => {
+            const course = state.courses.find(
+                course => course.id === payload)
+            if (course) {
+                course.isSelected = true;
+            }
+        },
+        unSelectCourse: (state, { payload }) => {
+            const course = state.courses.find(
+                course => course.id === payload)
+            if (course) {
+                course.isSelected = false;
+            }
+        }
+    },
+
     extraReducers: (builder) => {
         builder.addCase(fetchCourses.fulfilled, (state, action) => {
-            
-            state.courses = action.payload;
+            state.courses = action.payload.map(course => ({...course, isSelected: false}));
         });
         builder.addCase(logout, (state) => {
             state.courses = [];
-        })
+        });
     }
-})
+});
 
 export { fetchCourses };
+export const { selectCourse, unSelectCourse } = coursesSlice.actions;
 export default coursesSlice.reducer;
