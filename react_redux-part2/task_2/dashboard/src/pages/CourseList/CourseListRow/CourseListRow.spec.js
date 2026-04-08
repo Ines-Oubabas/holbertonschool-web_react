489c70855f07d69e0d3e5a95d99e82febf29571d
Inès Oubabas <143883134+Ines-Oubabas@users.noreply.github.com>
2026-04-08 16:34:33 +0200

    Refactor CourseListRow tests for clarity and accuracy

--- react_redux-part2/task_2/dashboard/src/pages/CourseList/CourseListRow/CourseListRow.spec.js
+++ react_redux-part2/task_2/dashboard/src/pages/CourseList/CourseListRow/CourseListRow.spec.js
@@ -1,116 +1,109 @@
-import { render, screen, within } from '@testing-library/react';
-import { test, expect } from "@jest/globals";
+import { render, screen, within, fireEvent } from '@testing-library/react';
 import CourseListRow from './CourseListRow';
 
-test('Should display 2 "th" element whenever the isHeader props set to true', () => {
+test('it should display 1 "th" element with colspan=2 when isHeader is true and textSecondCell is null', () => {
+  render(
+    <table>
+      <tbody>
+        <CourseListRow isHeader={true} textFirstCell="First" textSecondCell={null} />
+      </tbody>
+    </table>
+  )
+
+  const thElement = screen.getByRole('columnheader');
+
+  expect(thElement).toHaveAttribute('colSpan', '2');
+});
+
+test('it should display 2 "th" elements when isHeader is true and textSecondCell is not null', () => {
   render(
     <table>
       <tbody>
         <CourseListRow isHeader={true} textFirstCell="First" textSecondCell="Second" />
       </tbody>
     </table>
-  );
-  const trElement = screen.getAllByRole('columnheader');
-  expect(trElement).toHaveLength(2);
-});
+  )
 
-test('Should display 2 "td" element whenever the "isHeader" props set to false', () => {
-  render(
-    <table>
-      <tbody>
-        <CourseListRow isHeader={false} textFirstCell="Test" textSecondCell="Value" />
-      </tbody>
-    </table>
-  );
-  const trElement = screen.getAllByRole('cell');
-  expect(trElement).toHaveLength(2);
-});
-
-test('Should display 1 "th" element whenever the "isHeader" props set to true, and "textSecondCell" set to null', () => {
-  const props = {
-    isHeader: true,
-    textFirstCell: 'Test',
-    textSecondCell: null
-  };
-  render(
-    <table>
-      <tbody>
-        <CourseListRow {...props} />
-      </tbody>
-    </table>
-  );
-  const thElement = screen.getByRole('columnheader');
-  expect(thElement).toHaveAttribute('colSpan', '2');
-});
-
-test('Should display 2 "th" elements whenever the "isHeader" props set to true, and "textSecondCell" is not null', () => {
-  const props = {
-    isHeader: true,
-    textFirstCell: 'First',
-    textSecondCell: 'dummy title'
-  };
-  render(
-    <table>
-      <tbody>
-        <CourseListRow {...props} />
-      </tbody>
-    </table>
-  );
   const thElements = screen.getAllByRole('columnheader');
+
   expect(thElements).toHaveLength(2);
 });
 
-test('Should render 2 "td" elements inside a "tr" element whenever the "isHeader" prop is set to false', () => {
+test('it should render 2 "td" elements inside a "tr" element when isHeader is false', () => {
   render(
     <table>
       <tbody>
-        <CourseListRow isHeader={false} textFirstCell="Test" textSecondCell="Value" />
+        <CourseListRow isHeader={false} textFirstCell="Data1" textSecondCell="Data2" />
       </tbody>
     </table>
-  );
+  )
+
   const trElement = screen.getByRole('row');
-  const tdElement = within(trElement).getAllByRole('cell');
+  const tdElements = within(trElement).getAllByRole('cell');
+
   expect(trElement).toBeInTheDocument();
-  expect(tdElement).toHaveLength(2);
+  expect(tdElements).toHaveLength(2);
 });
 
-test('Should render a checkbox in the first cell when isHeader is false', () => {
+test('it should render a checkbox when isHeader is false', () => {
   render(
     <table>
       <tbody>
-        <CourseListRow 
-          isHeader={false} 
-          textFirstCell="ES6" 
-          textSecondCell="60"
-          id={1}
-          isChecked={false}
-          onChangeRow={() => {}}
-        />
+        <CourseListRow isHeader={false} textFirstCell="Data1" textSecondCell="Data2" />
       </tbody>
     </table>
-  );
+  )
+
   const checkbox = screen.getByRole('checkbox');
   expect(checkbox).toBeInTheDocument();
-  expect(checkbox).not.toBeChecked();
 });
 
-test('Should call onChangeRow with correct arguments when checkbox is clicked', () => {
-  const mockOnChangeRow = jest.fn();
+test('checkbox should be checked when isSelected is true', () => {
   render(
     <table>
       <tbody>
-        <CourseListRow 
-          isHeader={false} 
-          textFirstCell="ES6" 
-          textSecondCell="60"
+        <CourseListRow isHeader={false} isSelected={true} textFirstCell="Data1" textSecondCell="Data2" />
+      </tbody>
+    </table>
+  )
+  const checkbox = screen.getByRole('checkbox');
+
+  expect(checkbox).toBeChecked();
+});
+
+test('checkbox should be unchecked when isSelected is false', () => {
+  render(
+    <table>
+      <tbody>
+        <CourseListRow isHeader={false} isSelected={false} textFirstCell="Data1" textSecondCell="Data2" />
+      </tbody>
+    </table>
+  )
+  const checkbox = screen.getByRole('checkbox');
+
+  expect(checkbox).not.toBeChecked();
+
+});
+
+test('onChangeRow is called with correct parameters when checkbox is clicked', () => {
+  const mockOnChangeRow = jest.fn();
+
+  render(
+    <table>
+      <tbody>
+        <CourseListRow
           id={1}
-          isChecked={false}
+          isSelected={false}
           onChangeRow={mockOnChangeRow}
+          textFirstCell={'data1'}
+          textSecondCell={'data2'}
         />
       </tbody>
     </table>
   );
+
   const checkbox = screen.getByRole('checkbox');
-  checkbox.click();
+  fireEvent.click(checkbox);
+
   expect(mockOnChangeRow).toHaveBeenCalledWith(1, true);
 });
