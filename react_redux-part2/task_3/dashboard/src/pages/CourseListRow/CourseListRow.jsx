e2a69cb523597d076e0745d783d0a2e50190a401
Ines <inesoubabas56@gmail.com>
2026-04-09 14:41:16 +0200

    initial commit

---
+++ react_redux-part2/task_3/dashboard/src/pages/CourseListRow/CourseListRow.jsx
@@ -0,0 +1,50 @@
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  headerRow: {
+    backgroundColor: '#deb5b545',
+  },
+  row: {
+    backgroundColor: '#f5f5f5ab',
+  },
+});
+
+export default function CourseListRow({
+  isHeader = false,
+  textFirstCell = '',
+  textSecondCell = null,
+  isSelected = false,
+  changeRow = () => {},
+  id,
+}) {
+  const rowStyle = isHeader ? styles.headerRow : styles.row;
+
+  if (isHeader) {
+    return (
+      <tr className={css(rowStyle)}>
+        {textSecondCell === null ? (
+          <th colSpan="2">{textFirstCell}</th>
+        ) : (
+          <>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
+          </>
+        )}
+      </tr>
+    );
+  }
+
+  return (
+    <tr className={css(rowStyle)}>
+      <td>
+        <input
+          type="checkbox"
+          checked={isSelected}
+          onChange={(event) => changeRow(id, event.target.checked)}
+        />
+        {textFirstCell}
+      </td>
+      <td>{textSecondCell}</td>
+    </tr>
+  );
+}
